c6674e2b518b680d95f9394ff132d457ce9404db
liran <liran.alb@gmail.com>
2019-11-15 17:46:34 +0200

    initiate project

---
+++ slist.c
@@ -0,0 +1,5 @@
+//
+// Created by liran on 11/15/19.
+//
+
+#include "slist.h"
---
+++ slist.h
@@ -0,0 +1,8 @@
+//
+// Created by liran on 11/15/19.
+//
+
+#ifndef AUTOMATA_SLIST_H
+#define AUTOMATA_SLIST_H
+
+#endif //AUTOMATA_SLIST_H
