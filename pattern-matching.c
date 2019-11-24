9afdd26ae8d1e393148534b272c80952049283db
liran <liran.alb@gmail.com>
2019-11-24 17:17:06 +0200

    add goto_set

--- pattern-matching.c
+++ pattern-matching.c
@@ -33,8 +33,36 @@ int pm_init(pm_t *fsm) { //NEED TESTING
     return 0;
 }
 
+int pm_goto_set(pm_state_t *from_state, unsigned char symbol, pm_state_t *to_state) { //NEEDS TESTING
+    if(from_state == NULL || to_state == NULL) {
+        return -1;
+    }
+
+    pm_labeled_edge_t *newEdge = (pm_labeled_edge_t*)malloc(sizeof(pm_labeled_edge_t)); //creating a new arc
+    if (newEdge == NULL) { // checking arc allocation
+        printf("Cannot allocate initial memory for data\n");
+        return -1;
+    }
+
+    newEdge->label = symbol; // assigning symbol and to_state in the the arc
+    newEdge->state = to_state;
+
+    return slist_append(from_state->_transitions, newEdge); //add the new arc to the transition using appand
+
+}
+
 /*
- *
+
+
+struct pm_labeled_edge {
+    unsigned char label;  //the character "on" this edge
+    struct pm_state *state; //the destination state
+};
+
+typedef struct pm_labeled_edge pm_labeled_edge_t;
+
+
+
  * (slist_t*)malloc(sizeof(slist_t));
 pm_state_t
 struct pm_state
